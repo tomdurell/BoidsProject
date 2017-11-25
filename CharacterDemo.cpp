3cba4154d3f3e3c79adbd02efc27b2af94433cac
tomdurell <32387288+tomdurell@users.noreply.github.com>
2017-11-25 14:17:40 +0000

    No missles, up to main menu

---
+++ CharacterDemo.cpp
@@ -0,0 +1,292 @@
+//
+// Copyright (c) 2008-2016 the Urho3D project.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#include <Urho3D/Core/CoreEvents.h>
+#include <Urho3D/Core/ProcessUtils.h>
+#include <Urho3D/Engine/Engine.h>
+#include <Urho3D/Graphics/AnimatedModel.h>
+#include <Urho3D/Graphics/AnimationController.h>
+#include <Urho3D/Graphics/Camera.h>
+#include <Urho3D/Graphics/Light.h>
+#include <Urho3D/Graphics/Material.h>
+#include <Urho3D/Graphics/Octree.h>
+#include <Urho3D/Graphics/Renderer.h>
+#include <Urho3D/Graphics/Zone.h>
+#include <Urho3D/Input/Controls.h>
+#include <Urho3D/Input/Input.h>
+#include <Urho3D/IO/FileSystem.h>
+#include <Urho3D/Physics/CollisionShape.h>
+#include <Urho3D/Physics/PhysicsWorld.h>
+#include <Urho3D/Physics/RigidBody.h>
+#include <Urho3D/Resource/ResourceCache.h>
+#include <Urho3D/Scene/Scene.h>
+#include <Urho3D/UI/Font.h>
+#include <Urho3D/UI/Text.h>
+#include <Urho3D/UI/UI.h>
+//boids etc
+#include "Character.h"
+#include "CharacterDemo.h"
+#include "Touch.h"
+#include "Boids.h"
+
+//for the main menu
+#include <Urho3D/UI/LineEdit.h>
+#include <Urho3D/UI/Button.h>
+#include <Urho3D/UI/UIEvents.h>
+#include <Urho3D/UI/Window.h>
+#include <Urho3D/UI/CheckBox.h>
+
+#include <Urho3D/DebugNew.h>
+
+URHO3D_DEFINE_APPLICATION_MAIN(CharacterDemo)
+BoidSet setBoids;
+
+CharacterDemo::CharacterDemo(Context* context) :
+    Sample(context),
+    firstPerson_(false)
+{
+	//TUTORIAL: TODO
+}
+
+CharacterDemo::~CharacterDemo()
+{
+}
+
+void CharacterDemo::Start()
+{
+    // Execute base class startup
+    Sample::Start();
+    if (touchEnabled_)
+        touch_ = new Touch(context_, TOUCH_SENSITIVITY);
+	menuVisible = true;
+
+	
+
+	// Create static scene content
+	CreateScene();
+
+	// Subscribe to necessary events
+	SubscribeToEvents();
+	// Set the mouse mode to use in the sample
+	Sample::InitMouseMode(MM_RELATIVE);
+	//create the main menu
+	CreateMainMenu();
+
+}
+
+void CharacterDemo::CreateScene()
+{
+
+	//so we can access resources
+	ResourceCache* cache = GetSubsystem<ResourceCache>();
+	scene_ = new Scene(context_);
+	// Create scene subsystem components
+	scene_->CreateComponent<Octree>();
+	scene_->CreateComponent<PhysicsWorld>();
+
+	//Create camera node and component
+	cameraNode_ = new Node(context_);
+	Camera* camera = cameraNode_->CreateComponent<Camera>();
+	cameraNode_->SetPosition(Vector3(0.0f, 5.0f, 0.0f));
+	camera->SetFarClip(300.0f);
+
+	GetSubsystem<Renderer>()->SetViewport(0, new Viewport(context_,
+		scene_, camera));
+
+	// Create static scene content. First create a zone for ambient
+	//lighting and fog control
+	Node* zoneNode = scene_->CreateChild("Zone");
+	Zone* zone = zoneNode->CreateComponent<Zone>();
+	zone->SetAmbientColor(Color(0.15f, 0.15f, 0.15f));
+	zone->SetFogColor(Color(0.0f, 0.0f, 0.2f));
+	zone->SetFogStart(100.0f);
+	zone->SetFogEnd(300.0f);
+	zone->SetBoundingBox(BoundingBox(-1000.0f, 1000.0f));
+
+	// Create a directional light with cascaded shadow mapping
+	Node* lightNode = scene_->CreateChild("DirectionalLight");
+	lightNode->SetDirection(Vector3(0.3f, -0.5f, 0.425f));
+	Light* light = lightNode->CreateComponent<Light>();
+	light->SetLightType(LIGHT_DIRECTIONAL);
+	light->SetCastShadows(true);
+	light->SetShadowBias(BiasParameters(0.00025f, 0.5f));
+	light->SetShadowCascade(CascadeParameters(10.0f, 50.0f, 200.0f, 0.0f,
+		0.8f));
+	light->SetSpecularIntensity(0.5f);
+
+	// Create the floor object
+	Node* floorNode = scene_->CreateChild("Floor");
+	floorNode->SetPosition(Vector3(0.0f, -0.5f, 0.0f));
+	floorNode->SetScale(Vector3(200.0f, 1.0f, 200.0f));
+	StaticModel* object = floorNode->CreateComponent<StaticModel>();
+	object->SetModel(cache->GetResource<Model>("Models/Mushroom.mdl"));
+	object->SetMaterial(cache ->GetResource<Material>("Materials/Stone.xml"));
+	RigidBody* body = floorNode->CreateComponent<RigidBody>();
+	// Use collision layer bit 2 to mark world scenery. This is what we
+	//will raycast against to prevent camera from going
+	// inside geometry
+	body->SetCollisionLayer(2);
+	CollisionShape* shape = floorNode->CreateComponent<CollisionShape>();
+	shape->SetBox(Vector3::ONE);
+
+	
+	setBoids.Initialise(cache, scene_);
+	
+
+
+}
+
+void CharacterDemo::CreateCharacter()
+{
+	//TUTORIAL: TODO
+}
+
+void CharacterDemo::CreateInstructions()
+{
+	//TUTORIAL: TODO
+}
+
+void CharacterDemo::SubscribeToEvents()
+{
+	// Subscribe to Update event for setting the character controls
+	SubscribeToEvent(E_UPDATE, URHO3D_HANDLER(CharacterDemo, HandleUpdate));
+
+}
+
+void CharacterDemo::HandleUpdate(StringHash eventType, VariantMap& eventData)
+{
+	using namespace Update;
+	// Take the frame time step, which is stored as a float
+	float timeStep = eventData[P_TIMESTEP].GetFloat();
+	// Do not move if the UI has a focused element (the console)
+	if (GetSubsystem<UI>()->GetFocusElement()) return;
+	Input* input = GetSubsystem<Input>();
+	// Movement speed as world units per second
+	const float MOVE_SPEED = 20.0f;
+	// Mouse sensitivity as degrees per pixel
+	const float MOUSE_SENSITIVITY = 0.1f;
+	// Use this frame's mouse motion to adjust camera node yaw and pitch.
+	// Clamp the pitch between -90 and 90 degrees
+	IntVector2 mouseMove = input->GetMouseMove();
+	yaw_ += MOUSE_SENSITIVITY * mouseMove.x_;
+	pitch_ += MOUSE_SENSITIVITY * mouseMove.y_;
+	pitch_ = Clamp(pitch_, -90.0f, 90.0f);
+	// Construct new orientation for the camera scene node from
+	// yaw and pitch. Roll is fixed to zero
+	cameraNode_->SetRotation(Quaternion(pitch_, yaw_, 0.0f));
+	// Read WASD keys and move the camera scene node to the corresponding
+	// direction if they are pressed, use the Translate() function
+	// (default local space) to move relative to the node's orientation.
+	if (input->GetKeyDown(KEY_W))
+		cameraNode_->Translate(Vector3::FORWARD * MOVE_SPEED *
+			timeStep);
+	if (input->GetKeyDown(KEY_S))
+		cameraNode_->Translate(Vector3::BACK * MOVE_SPEED * timeStep);
+	if (input->GetKeyDown(KEY_A))
+		cameraNode_->Translate(Vector3::LEFT * MOVE_SPEED * timeStep);
+	if (input->GetKeyDown(KEY_D))
+		cameraNode_->Translate(Vector3::RIGHT * MOVE_SPEED * timeStep);
+	if (input->GetKeyPress(KEY_M))
+		menuVisible = !menuVisible;
+
+	setBoids.Update(timeStep, scene_);
+	
+
+}
+
+void CharacterDemo::HandlePostUpdate(StringHash eventType, VariantMap& eventData)//MISSING A HANDLE POST UPDATE CALL, WHERE SHOULD IT GO?
+{
+	// menu visible & invisible
+	UI* ui = GetSubsystem<UI>();
+	Input* input = GetSubsystem<Input>();
+	ui->GetCursor()->SetVisible(menuVisible);
+	window_->SetVisible(menuVisible);
+}
+
+void CharacterDemo::CreateMainMenu()
+{
+	// Set the mouse mode to use in the sample
+	InitMouseMode(MM_RELATIVE);
+	ResourceCache* cache = GetSubsystem<ResourceCache>();
+	UI* ui = GetSubsystem<UI>();
+	UIElement* root = ui->GetRoot();
+	XMLFile* uiStyle = cache->GetResource<XMLFile>("UI/DefaultStyle.xml");
+	root->SetDefaultStyle(uiStyle); //need to set default ui style
+	//Create a Cursor UI element.We want to be able to hide and show the main menu at will.When hidden, the mouse will control the camera, and when visible, the
+	//mouse will be able to interact with the GUI.
+	SharedPtr<Cursor> cursor(new Cursor(context_));
+	cursor->SetStyleAuto(uiStyle);
+	ui->SetCursor(cursor);
+	// Create the Window and add it to the UI's root node
+	window_ = new Window(context_);
+	root->AddChild(window_);
+	// Set Window size and layout settings
+	window_->SetMinWidth(384);
+	window_->SetLayout(LM_VERTICAL, 6, IntRect(6, 6, 6, 6));
+	window_->SetAlignment(HA_CENTER, VA_CENTER);
+	window_->SetName("Window");
+	window_->SetStyleAuto();
+
+	//populate the window with buttons and line edits
+	
+	window_->AddChild(CreateButton("Button 1", 24, window_));
+	window_->AddChild(CreateButton("Button 2", 24, window_));
+	Button* QuitButton = CreateButton("Quit", 24, window_);
+	window_->AddChild(QuitButton);
+	window_->AddChild(CreateLineEdit("LineText", 24, window_));
+
+	SubscribeToEvent(QuitButton, E_RELEASED, URHO3D_HANDLER(CharacterDemo, HandleQuit));
+
+}
+
+Button* CharacterDemo::CreateButton(const String& text, int pHeight, Urho3D::Window* whichWindow)
+{
+	
+	Button* button = whichWindow->CreateChild<Button>();
+	button->SetMinHeight(pHeight);
+	button->SetStyleAuto();
+	Text* buttonText = button->CreateChild<Text>();
+	ResourceCache* cache = GetSubsystem<ResourceCache>();
+	Font* font = cache->GetResource<Font>("Fonts/Anonymous Pro.ttf");
+	buttonText->SetFont(font, 12);
+	buttonText->SetAlignment(HA_CENTER, VA_CENTER);
+	buttonText->SetText(text);
+	return button;
+	
+}
+
+LineEdit* CharacterDemo::CreateLineEdit(const String& text, int pHeight, Urho3D::Window*
+	whichWindow) 
+{
+	LineEdit* lineEdit = whichWindow->CreateChild<LineEdit>();
+	lineEdit->SetMinHeight(pHeight);
+	lineEdit->SetAlignment(HA_CENTER, VA_CENTER);
+	lineEdit->SetText(text);
+	lineEdit->SetStyleAuto();
+	return lineEdit;
+}
+
+void CharacterDemo::HandleQuit(StringHash eventType, VariantMap& eventData)
+{
+	// quitting code here
+	engine_->Exit();
+}
